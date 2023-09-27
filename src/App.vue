3a9fa9b3b7e76cb298c6a2fb10e5264453a633c5
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-09-27 16:27:55 +0200

    START project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  }
+}
+
+</script>
+
+<template>
+  <HeaderComponent />
+  <MainComponent />
+  <FooterComponent />
+</template>
+
+<style scoped></style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'FooterComponent',
+    data() {
+        return {
+            title: 'Footer Component'
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h2>
+            {{ title }}
+        </h2>
+    </header>
+</template>
+
+<style scoped>
+h2 {
+    color: blue;
+}
+</style>
